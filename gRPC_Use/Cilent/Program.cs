270ce3d986d14ede6803eca980b83599b3461239
liliangouw <1803443590@qq.com>
2023-01-08 18:06:54 +0800

    初始化仓库并添加gRP代码示例

---
+++ gRPC_Use/Cilent/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Cilent;
+
+Console.WriteLine("Hello, World!");
+
+//客户端调用gRPC
+//1.引入Nuget包：Grpc.Net.Client、Grpc.Tools、Google.Protobuf
+//2.拷贝服务端的Proto文件，修改项目文件为Cilent
+//3.调用
+await gRPCRequest.TestHello();
+await gRPCRequest.TestCount();
+
+//支持AOP
+//1.引入Grpc.Core.Api
+//2.通过Intercepter扩展
+Console.ReadKey();
---
+++ gRPC_Use/Cilent/gRPCRequest.cs
@@ -0,0 +1,36 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Net.Client;
+using gRPC_Use;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cilent
+{
+    internal class gRPCRequest
+    {
+        public static async Task TestHello()
+        {
+            using(var channel = GrpcChannel.ForAddress("https://localhost:7042"))
+            {
+                var client=new Greeter.GreeterClient(channel);
+                var reply = await client.SayHelloAsync(new HelloRequest
+                {
+                    Name="你好"
+                });
+                Console.WriteLine("Greeter服务器返回数据:"+reply.Message);
+            }
+        }
+        public static async Task TestCount()
+        {
+            using (var channel = GrpcChannel.ForAddress("https://localhost:7042"))
+            {
+                var client = new Greeter.GreeterClient(channel);
+                var reply = await client.CountAsync(new Empty());
+                Console.WriteLine("Greeter服务器返回数据:" + reply.Count);
+            }
+        }
+    }
+}
